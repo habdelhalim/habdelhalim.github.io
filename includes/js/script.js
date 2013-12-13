1c17e486065b208746e3cbb9e2a87324e553e62a
Hasan Abdel Halim <habdelhalim@souq.com>
2013-12-13 17:48:17 +0300

    remove unused resources

--- includes/js/script.js
+++ includes/js/script.js
@@ -3,8 +3,16 @@
 My Custom JS
 ============
 
+Author: Hasan Abdel Halim
 Author:  Brad Hussey
 Updated: August 2013
 Notes:	 Hand coded for Udemy.com
 
 */
+
+
+$(function () {
+	$('a.pop').click(function (e) {
+		e.preventDefault();
+	});
+});
