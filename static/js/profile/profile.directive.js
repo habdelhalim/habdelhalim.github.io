dc583bf0931c5ae92f55dc882aede1b9f5dfe7ee
Hasan Abdel Halim <hasana@gmail.com>
2017-04-01 00:56:38 +0300

    moved many parts into their own directives

--- static/js/profile/profile.directive.js
+++ static/js/profile/profile.directive.js
@@ -5,6 +5,27 @@
   "use strict";
 
   var module = angular.module('profile.directive', []);
+  module.directive('profileImage', function () {
+    return {
+      restrict: 'E',
+      scope: {
+        profile: '='
+      },
+      template: '<a href="{{profile.publicProfileUrl}}" class="thumbnail" rel="Hasan Abdel Halim\'s profile"> <img src="/static/img/profile.jpg" alt="Hasan Abdel Halim"> </a>'
+    }
+  });
+
+
+  module.directive('job', function () {
+    return {
+      restrict: 'E',
+      scope: {
+        position: '='
+      },
+      templateUrl: 'static/js/profile/profile.job.html'
+    }
+  });
+
   module.directive('jobItem', function () {
     return {
       restrict: 'E',
@@ -15,13 +36,43 @@
     }
   });
 
-  module.directive('job', function () {
+  module.directive('skills', function () {
     return {
       restrict: 'E',
       scope: {
-        position: '='
+        skills: '='
       },
-      templateUrl: 'static/js/profile/profile.job.html'
+      templateUrl: 'static/js/profile/profile.skills.html'
+    }
+  });
+
+  module.directive('aboutMe', function () {
+    return {
+      restrict: 'E',
+      scope: {
+        profile: '='
+      },
+      templateUrl: 'static/js/profile/profile.aboutme.html'
+    }
+  });
+
+  module.directive('education', function () {
+    return {
+      restrict: 'E',
+      scope: {
+        educations: '='
+      },
+      templateUrl: 'static/js/profile/profile.education.html'
+    }
+  });
+
+  module.directive('language', function () {
+    return {
+      restrict: 'E',
+      scope: {
+        languages: '='
+      },
+      templateUrl: 'static/js/profile/profile.language.html'
     }
   });
 
